2cf2aa2cf090de23125a653f7bf466e4da9873fc
Mutuking <kawaguchi.2525.haruki@gmail.com>
2024-07-30 16:00:07 +0900

    04_04_応用課題

--- main.cpp
+++ main.cpp
@@ -30,17 +30,17 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
     Vector3 cameraPosition{ 0.0f, 1.0f, -5.0f };
     Vector2Int clickPos{};
 
+    Plane plane{};
+    plane.normal = Normalize({ -0.2f, 1.2f, -0.3f });
+    plane.distance = 0.0f;
+
     Ball ball{};
-    ball.acceleration = { 0.0f, -9.8f, 0.0f };
     ball.position = { 0.8f, 1.2f, 0.3f };
-    ball.velocity = { 0.0f, 0.0f, 0.0f };
     ball.mass = 2.0f;
     ball.radius = 0.05f;
     ball.color = 0xFFFFFFFF;
-
-    Plane plane{};
-    plane.normal = Normalize({ -0.2f, 0.9f, -0.3f });
-    plane.distance = 0.0f;
+    ball.acceleration = { 0.0f, -9.8f, 0.0f };
+    ball.velocity = { 0.0f, 0.0f, 0.0f };
 
     float deltaTime = 1.0f / 60.0f;
     bool isStart = false;
@@ -64,17 +64,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
         Matrix4x4 viewportMatrix = MakeViewportMatrix(0, 0, float(kWindowWidth), float(kWindowHeight), 0.0f, 1.0f);
         Matrix4x4 viewProjectionMatrix = viewMatrix * projectionMatrix;
 
-        //if (isStart) {
-        //    ball.velocity += ball.acceleration * deltaTime;
-        //    ball.position += ball.velocity * deltaTime;
-        //    if (IsCollision(Sphere{ ball.position, ball.radius }, plane)) {
-        //        Vector3 reflected = Reflect(ball.velocity, plane.normal);
-        //        Vector3 projectToNormal = Project(reflected, plane.normal);
-        //        Vector3 movingDirection = reflected - projectToNormal;
-        //        ball.velocity = projectToNormal * 0.99f + movingDirection;
-        //    }
-        //}
-
         if (isStart) {
             ball.velocity += ball.acceleration * deltaTime;
             ball.position += ball.velocity * deltaTime;
@@ -91,6 +80,10 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
                 // 新しい進行方向を計算 (法線方向の速度を減衰させる)
                 ball.velocity = projectToNormal * 0.8f + tangent;
             }
+        }
+
+        if (ball.position.y <= -3.0f) {
+            isStart = false;
         }
 
         ///
@@ -116,7 +109,9 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
         ImGui::End();
 
         DrawGrid(viewProjectionMatrix, viewportMatrix);
-        DrawPoint(ball.position, viewProjectionMatrix, viewportMatrix, 0xFFFFFFFF);
+        if (isStart) {
+            DrawPoint(ball.position, viewProjectionMatrix, viewportMatrix, 0xFFFFFFFF);
+        }
         DrawPlane(plane, viewProjectionMatrix, viewportMatrix, 0xFFFFFFFF);
 
         ///
